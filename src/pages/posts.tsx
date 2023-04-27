d9fd576ad9f8708d8bf5322c681d089c5e01db6d
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2023-04-28 02:44:56 +0800

    fix page

--- src/pages/posts.tsx
+++ src/pages/posts.tsx
@@ -1,7 +1,36 @@
-export default function PostsListsPage() {
+import { getXataClient, Posts } from "../xata";
+// We'll assign the client to xata to make it easier to call
+const xata = getXataClient();
+
+// We will make the query to Xata on the server.
+// This will allow the query to read the API_KEY from the .env file
+export const getServerSideProps = async () => {
+  const records: Posts[] = await xata.db.Posts.getMany();
+  return {
+    props: {
+      records: records.map((record) => ({
+        ...record,
+        // We need to convert our date from an object to a string to use it here
+        // If you didn't set up your pubDate column as "not null" Typescript may complain, so we'll add ? after pubDate for this example
+        pubDate: record.pubDate?.toDateString(),
+      })),
+    },
+  };
+};
+
+// Pass the props defined above to the page and map against it
+export default function PostsListPage({ records }: { records: Posts[] }) {
   return (
     <>
       <h1>My xata posts</h1>
+      {records.map((record: Posts) => (
+        <div key={record.id}>
+          <h2>{record.Title}</h2>
+          <p>{record.id}</p>
+          <p>{record.body}</p>
+          <p>{record.pubDate}</p>
+        </div>
+      ))}
     </>
   );
 }
